a065c61ec1223895465d1d2db564957540251a86
Hannah Riley <riley.hannahm@gmail.com>
2020-06-26 16:22:41 -0700

    added edit meal and delete meal actions

--- src/actions/mealActions.js
+++ src/actions/mealActions.js
@@ -4,15 +4,14 @@ export const addMeal = (meal) => ({
   payload: meal
 });
 
-//what part of a meal would i even want to edit? i think this can be deleted
-export const EDIT_MEAL = 'EDIT_MEAL';
-export const editMeal = (meal) => ({
-  type: 'EDIT_MEAL',
-  payload: meal
-});
-
 export const DELETE_MEAL = 'DELETE_MEAL';
 export const deleteMeal = (meal) => ({
   type: 'DELETE_MEAL',
+  payload: meal
+});
+
+export const EDIT_MEAL = 'EDIT_MEAL';
+export const editMeal = (meal) => ({
+  type: 'EDIT_MEAL',
   payload: meal
 });
