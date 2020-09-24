41b2bda505733b93bc275344c0e2b161c27bc1ae
Hannah Riley <riley.hannahm@gmail.com>
2020-09-24 12:01:18 -0700

    updated landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
-  
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+
+
   return (
     <>
       <h2>Decision Maker</h2>
       <p>where life becomes easy</p>
+      
+      <form>
+        <p>pick a category</p>
+        <select name="categories" id="categorySelector">
+          <option value="plants">Plants</option>
+          <option value="meals">Meals</option>
+        </select>
+        <p>go!</p>
+      </form>
     </>
   );
 };
